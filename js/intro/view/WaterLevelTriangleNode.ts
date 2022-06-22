7cd9eb959654250bf9b93ff853f0bf37f0b9a479
Marla Schulz <marlaa.schulz@gmail.com>
2022-06-22 11:08:41 -0600

    Move TriangleNode from proportion-playground to scenery-phet and transfer to ts, see: https://github.com/phetsims/scenery-phet/issues/740

--- js/intro/view/WaterLevelTriangleNode.ts
+++ js/intro/view/WaterLevelTriangleNode.ts
@@ -3,6 +3,8 @@
 /**
  * Representation for the triangle that adjusts the water level in 3D cups.
  *
+ * The triangle code in this Node is duplicated. Transferring to scenery-phet: https://github.com/phetsims/scenery-phet/issues/748
+ *
  * @author Marla Schulz (PhET Interactive Simulations)
  * @author Sam Reid (PhET Interactive Simulations)
  */
