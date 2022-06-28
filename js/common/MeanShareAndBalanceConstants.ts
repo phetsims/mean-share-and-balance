5cb80c2813c7a9f563c6ad90c6a84b866b6fc550
pixelzoom <cmalley@pixelzoom.com>
2022-06-28 15:01:30 -0600

    add REVIEW comments, #41

--- js/common/MeanShareAndBalanceConstants.ts
+++ js/common/MeanShareAndBalanceConstants.ts
@@ -21,15 +21,18 @@ const MeanShareAndBalanceConstants = {
   CUP_RANGE_MIN: 0,
   CUP_RANGE_MAX: 1,
   WATER_LEVEL_DEFAULT: 0.5,
+  //REVIEW Should this be renamed PIPE_LINE_WIDTH, and be used to set lineWidth for the pipe?
   PIPE_STROKE_WIDTH: 1,
   PIPE_LENGTH: 50,
   CONTROLS_VERTICAL_MARGIN: 30,
   CONTROLS_HORIZONTAL_MARGIN: 15,
 
   // Sets center Y value for cups.
+  //REVIEW If these set centerY, then they should be named CUPS_3D_CENTER_Y and CUPS_2D_CENTER_Y. The current names correspond to a Node's y property.
   CUPS_3D_Y_VALUE: 525,
   CUPS_2D_Y_VALUE: 275,
 
+  //REVIEW These names are a little vague/incorrect. MOUSE_AREA_DILATION and TOUCH_AREA_DILATION dilation would be clearer.
   MOUSE_DILATION: 5,
   TOUCH_DILATION: 10,
 
