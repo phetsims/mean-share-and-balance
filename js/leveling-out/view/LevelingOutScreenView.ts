b9792d7b91577ae16c6eeb778dc41ffd50a31526
Marla Schulz <marlaa.schulz@gmail.com>
2022-06-29 13:53:29 -0600

    Add basic controls, see: https://github.com/phetsims/mean-share-and-balance/issues/52

--- js/leveling-out/view/LevelingOutScreenView.ts
+++ js/leveling-out/view/LevelingOutScreenView.ts
@@ -12,6 +12,11 @@ import EmptyObjectType from '../../../../phet-core/js/types/EmptyObjectType.js';
 import MeanShareAndBalanceScreenView, { MeanShareAndBalanceScreenViewOptions } from '../../common/view/MeanShareAndBalanceScreenView.js';
 import meanShareAndBalance from '../../meanShareAndBalance.js';
 import LevelingOutModel from '../model/LevelingOutModel.js';
+import AccordionBox from '../../../../sun/js/AccordionBox.js';
+import { Rectangle, Text } from '../../../../scenery/js/imports.js';
+import NumberSpinner from '../../../../sun/js/NumberSpinner.js';
+import Property from '../../../../axon/js/Property.js';
+import MeanShareAndBalanceConstants from '../../common/MeanShareAndBalanceConstants.js';
 
 type SelfOptions = EmptyObjectType;
 
@@ -21,11 +26,30 @@ export default class LevelingOutScreenView extends MeanShareAndBalanceScreenView
 
   public constructor( levelingOutModel: LevelingOutModel, providedOptions?: LevelingOutScreenViewOptions ) {
 
-    const options = optionize<LevelingOutScreenViewOptions, SelfOptions, MeanShareAndBalanceScreenViewOptions>()( {
-
-    }, providedOptions );
+    const options = optionize<LevelingOutScreenViewOptions, SelfOptions, MeanShareAndBalanceScreenViewOptions>()( {}, providedOptions );
 
     super( levelingOutModel, options );
+
+    const meanNode = new Rectangle( 0, 0, 40, 20, { fill: 'brown' } );
+    const meanAccordionBox = new AccordionBox( meanNode, {
+      titleNode: new Text( 'Mean', { fontSize: 15, maxWidth: MeanShareAndBalanceConstants.MAX_CONTROLS_TEXT_WIDTH } ),
+      expandedProperty: levelingOutModel.isMeanAccordianExpandedProperty,
+      tandem: options.tandem.createTandem( 'meanAccordionBox' )
+    } );
+
+    const numberOfPeopleText = new Text( 'Number of People', { fontSize: 15, maxWidth: MeanShareAndBalanceConstants.MAX_CONTROLS_TEXT_WIDTH } );
+
+    const numberOfPeopleNumberSpinner = new NumberSpinner(
+      levelingOutModel.numberOfPeopleProperty,
+      new Property( levelingOutModel.numberOfPeopleRange ), {
+        arrowsPosition: 'leftRight',
+        tandem: options.tandem.createTandem( 'numberOfPeopleNumberSpinner' )
+      } );
+
+
+    this.controlsVBox.addChild( meanAccordionBox );
+    this.numberSpinnerVBox.addChild( numberOfPeopleText );
+    this.numberSpinnerVBox.addChild( numberOfPeopleNumberSpinner );
   }
 
 }
