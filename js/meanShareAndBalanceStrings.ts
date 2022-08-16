e196629ec80f1c71adeca3c3108ae8634bcfda93
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:34 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/meanShareAndBalanceStrings.ts
+++ js/meanShareAndBalanceStrings.ts
@@ -5,23 +5,34 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import meanShareAndBalance from './meanShareAndBalance.js';
 
 type StringsType = {
   'mean-share-and-balance': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'intro': string;
+    'introProperty': TReadOnlyProperty<string>;
     'levelingOut': string;
+    'levelingOutProperty': TReadOnlyProperty<string>;
   };
   'predictMean': string;
+  'predictMeanProperty': TReadOnlyProperty<string>;
   'mean': string;
+  'meanProperty': TReadOnlyProperty<string>;
   'tickMarks': string;
+  'tickMarksProperty': TReadOnlyProperty<string>;
   'sync': string;
+  'syncProperty': TReadOnlyProperty<string>;
   'numberOfCups': string;
+  'numberOfCupsProperty': TReadOnlyProperty<string>;
   'introQuestion': string;
+  'introQuestionProperty': TReadOnlyProperty<string>;
   'levelingOutQuestion': string;
+  'levelingOutQuestionProperty': TReadOnlyProperty<string>;
 };
 
 const meanShareAndBalanceStrings = getStringModule( 'MEAN_SHARE_AND_BALANCE' ) as StringsType;
