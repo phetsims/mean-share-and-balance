827a08a24f868b3b8950c106ba355eb1125206db
Marla Schulz <marlaa.schulz@gmail.com>
2022-09-01 16:57:55 -0700

    Stack chocolate bars upwards, see: https://github.com/phetsims/mean-share-and-balance/issues/52

--- js/leveling-out/view/ChocolateBarsContainerNode.ts
+++ js/leveling-out/view/ChocolateBarsContainerNode.ts
@@ -11,27 +11,28 @@
 
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
-import { Image, NodeOptions, NodeTranslationOptions, VBox, VBoxOptions } from '../../../../scenery/js/imports.js';
+import { Node, NodeOptions, NodeTranslationOptions, VBoxOptions } from '../../../../scenery/js/imports.js';
 import meanShareAndBalance from '../../meanShareAndBalance.js';
 import Plate from '../model/Plate.js';
 import MeanShareAndBalanceConstants from '../../common/MeanShareAndBalanceConstants.js';
-import chocolateBar_png from '../../../images/chocolateBar_png.js';
+import DraggableChocolateNode from './DraggableChocolateNode.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 
-type ChocolateBarsContainerNodeOptions = StrictOmit<VBoxOptions, keyof NodeTranslationOptions>;
+type ChocolateBarsContainerNodeOptions = StrictOmit<VBoxOptions, keyof NodeTranslationOptions> & PickRequired<NodeOptions, 'tandem'>;
 
-export default class ChocolateBarsContainerNode extends VBox {
-  public constructor( plate: Plate, providedOptions?: ChocolateBarsContainerNodeOptions ) {
+export default class ChocolateBarsContainerNode extends Node {
+  public constructor( plate: Plate, providedOptions: ChocolateBarsContainerNodeOptions ) {
     const options = optionize<ChocolateBarsContainerNodeOptions, EmptySelfOptions, NodeOptions>()( {
       x: plate.position.x,
       y: plate.position.y,
-      scale: 0.08,
       visibleProperty: plate.isActiveProperty
     }, providedOptions );
 
-    const chocolateBars: Array<Image> = [];
+    const chocolateBars: Array<Node> = [];
     for ( let i = 0; i < MeanShareAndBalanceConstants.MAX_NUMBER_OF_CHOCOLATES; i++ ) {
-      const chocolateBar = new Image( chocolateBar_png );
-      chocolateBar.y = ( MeanShareAndBalanceConstants.CHOCOLATE_HEIGHT + 5 ) * i;
+      const chocolateBar = new DraggableChocolateNode( { tandem: options.tandem.createTandem( 'chocolateBar' ) } );
+      chocolateBar.y = ( chocolateBar.height + 1.5 ) * -i;
+
       chocolateBars.push( chocolateBar );
     }
 
@@ -42,7 +43,7 @@ export default class ChocolateBarsContainerNode extends VBox {
     } );
 
     options.children = chocolateBars;
-    options.spacing = 20;
+    options.excludeInvisibleChildrenFromBounds = false;
     super( options );
   }
 }
--- js/leveling-out/view/DraggableChocolateNode.ts
+++ js/leveling-out/view/DraggableChocolateNode.ts
@@ -9,16 +9,38 @@
  */
 
 import meanShareAndBalance from '../../meanShareAndBalance.js';
-import { DragListener, Image, Node } from '../../../../scenery/js/imports.js';
+import { DragListener, Image, Node, NodeOptions } from '../../../../scenery/js/imports.js';
 import chocolateBar_png from '../../../images/chocolateBar_png.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import Property from '../../../../axon/js/Property.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+
+type DraggableChocolateNodeOptions = NodeOptions & PickRequired<NodeOptions, 'tandem'>;
 
 export default class DraggableChocolateNode extends Node {
 
-  public constructor() {
+  public constructor( providedOptions: DraggableChocolateNodeOptions ) {
 
+    const options = providedOptions;
+
+    const positionProperty = new Property( new Vector2( 0, 0 ) );
     const chocolateBar = new Image( chocolateBar_png, { scale: 0.05 } );
-    const chocolateBarDragListener = new DragListener();
-   super( { children: [ chocolateBar ], inputListeners: [ chocolateBarDragListener ] } );
+    const chocolateBarDragListener = new DragListener( {
+      positionProperty: positionProperty,
+      tandem: options.tandem.createTandem( 'chocolateBarDragListener' )
+    } );
+
+    const combinedOptions = combineOptions<NodeOptions>( {
+        children: [ chocolateBar ],
+        inputListeners: [ chocolateBarDragListener ]
+    }, options );
+    super( combinedOptions );
+
+    positionProperty.link( position => {
+      this.x = position.x;
+      this.y = position.y;
+    } );
   }
 }
 
