3480d35782b3527b4fef3f0b43130f5604f2e1a8
Marla Schulz <marlaa.schulz@gmail.com>
2022-11-01 10:55:49 -0600

    Remove leveling out screen for RC/Prototype

--- js/mean-share-and-balance-main.ts
+++ js/mean-share-and-balance-main.ts
@@ -12,7 +12,7 @@ import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import MeanShareAndBalanceStrings from './MeanShareAndBalanceStrings.js';
 import IntroScreen from './intro/IntroScreen.js';
-import LevelingOutScreen from './leveling-out/LevelingOutScreen.js';
+// import LevelingOutScreen from './leveling-out/LevelingOutScreen.js';
 
 const simOptions: SimOptions = {
 
@@ -31,8 +31,8 @@ const simOptions: SimOptions = {
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
   const sim = new Sim( MeanShareAndBalanceStrings[ 'mean-share-and-balance' ].titleStringProperty, [
-    new IntroScreen( { tandem: Tandem.ROOT.createTandem( 'introScreen' ) } ),
-    new LevelingOutScreen( { tandem: Tandem.ROOT.createTandem( 'levelingOutScreen' ) } )
+    new IntroScreen( { tandem: Tandem.ROOT.createTandem( 'introScreen' ) } )
+    // new LevelingOutScreen( { tandem: Tandem.ROOT.createTandem( 'levelingOutScreen' ) } )
   ], simOptions );
   sim.start();
 } );
