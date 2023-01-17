97704953f58662a2bffa9d9ec508a908739eb959
phet-dev <phetadmin@gmail.com>
2023-01-17 04:27:55 -0700

    update copyright dates from daily grunt work

--- js/leveling-out/view/DraggableChocolate.ts
+++ js/leveling-out/view/DraggableChocolate.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * An individual chocolate bar node. These chocolate bars are draggable, and must stay within the notebook paper bounds.
