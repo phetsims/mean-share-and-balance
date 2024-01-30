cb1324f1eac2a22bfd387320cc6ff667bd003693
Marla Schulz <marlaa.schulz@gmail.com>
2024-01-30 13:24:18 -0700

    Turn on dynamic locale, see: https://github.com/phetsims/mean-share-and-balance/issues/143

--- js/MeanShareAndBalanceStrings.ts
+++ js/MeanShareAndBalanceStrings.ts
@@ -10,46 +10,27 @@ import meanShareAndBalance from './meanShareAndBalance.js';
 
 type StringsType = {
   'mean-share-and-balance': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
   };
   'screen': {
-    'intro': string;
     'introStringProperty': LocalizedStringProperty;
-    'levelingOut': string;
     'levelingOutStringProperty': LocalizedStringProperty;
   };
-  'predictMean': string;
   'predictMeanStringProperty': LocalizedStringProperty;
-  'mean': string;
   'meanStringProperty': LocalizedStringProperty;
-  'tickMarks': string;
   'tickMarksStringProperty': LocalizedStringProperty;
-  'cupWaterLevel': string;
   'cupWaterLevelStringProperty': LocalizedStringProperty;
-  'sync': string;
   'syncStringProperty': LocalizedStringProperty;
-  'numberOfCups': string;
   'numberOfCupsStringProperty': LocalizedStringProperty;
-  'introQuestion': string;
   'introQuestionStringProperty': LocalizedStringProperty;
-  'levelingOutQuestion': string;
   'levelingOutQuestionStringProperty': LocalizedStringProperty;
-  'numberOfPeople': string;
   'numberOfPeopleStringProperty': LocalizedStringProperty;
-  'meanEquals': string;
   'meanEqualsStringProperty': LocalizedStringProperty;
-  'totalCandyBarsPattern': string;
   'totalCandyBarsPatternStringProperty': LocalizedStringProperty;
-  'bar': string;
   'barStringProperty': LocalizedStringProperty;
-  'bars': string;
   'barsStringProperty': LocalizedStringProperty;
-  'totalCookiesPattern': string;
   'totalCookiesPatternStringProperty': LocalizedStringProperty;
-  'cookie': string;
   'cookieStringProperty': LocalizedStringProperty;
-  'cookies': string;
   'cookiesStringProperty': LocalizedStringProperty;
 };
 
