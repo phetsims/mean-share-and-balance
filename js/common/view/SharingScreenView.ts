3ccc28be4b4fba522e24342834edb9521243d0c1
phet-dev <phetadmin@gmail.com>
2024-02-03 03:21:50 -0700

    update copyright dates from daily grunt work

--- js/common/view/SharingScreenView.ts
+++ js/common/view/SharingScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 /**
  * The base screen view for the Leveling Out Screen and the Fair Share Screen.
  * Both screens have:
