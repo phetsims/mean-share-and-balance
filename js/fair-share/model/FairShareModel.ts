b5d2ba2da2f632aeae35b94f35cfcb395240cc1f
jbphet <jpblanco@yahoo.com>
2024-02-06 15:41:50 -0600

    add the enumeration Property for the notepad display mode, see https://github.com/phetsims/mean-share-and-balance/issues/149

--- js/fair-share/model/FairShareModel.ts
+++ js/fair-share/model/FairShareModel.ts
@@ -13,15 +13,43 @@ import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import meanShareAndBalance from '../../meanShareAndBalance.js';
 import SharingModel, { SharingModelOptions } from '../../common/model/SharingModel.js';
 import Apple from './Apple.js';
+import EnumerationValue from '../../../../phet-core/js/EnumerationValue.js';
+import Enumeration from '../../../../phet-core/js/Enumeration.js';
+import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
 
 type SelfOptions = EmptySelfOptions;
 type FairShareModelOptions = SelfOptions & PickRequired<SharingModelOptions, 'tandem'>;
 
+class NotepadOrganizationMode extends EnumerationValue {
+
+  // The information is displayed such that it is in sync with what is on the plates on the table.
+  public static readonly SYNC = new NotepadOrganizationMode();
+
+  // The snacks are collected into a single display that doesn't show the plates.
+  public static readonly COLLECT = new NotepadOrganizationMode();
+
+  // The total amount of snacks on the table are shared evenly between the plates in the notepad.
+  public static readonly SHARE = new NotepadOrganizationMode();
+
+  // Gets a list of keys, values and mapping between them.  For use in EnumerationProperty and PhET-iO
+  public static readonly enumeration = new Enumeration( NotepadOrganizationMode, {
+    phetioDocumentation: 'Describes the way in which the information in the notepad is displayed.'
+  } );
+}
+
 export default class FairShareModel extends SharingModel<Apple> {
 
-  public constructor( providedOptions?: FairShareModelOptions ) {
+  // An enumeration property that controls the way in which the quantity of snacks on the table are displayed on the
+  // notepad.  See the enumeration for more information on the possible modes.
+  public readonly notepadOrganizationModeProperty: EnumerationProperty<NotepadOrganizationMode>;
+
+  public constructor( providedOptions: FairShareModelOptions ) {
     const options = optionize<FairShareModelOptions, SelfOptions, SharingModelOptions>()( {}, providedOptions );
     super( options );
+    this.notepadOrganizationModeProperty = new EnumerationProperty( NotepadOrganizationMode.SYNC, {
+      tandem: providedOptions.tandem.createTandem( 'notepadOrganizationModeProperty' ),
+      phetioFeatured: true
+    } );
   }
 }
 
