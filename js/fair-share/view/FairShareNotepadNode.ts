b8322da54055090fb932a90bc0de7f633ead5bdf
phet-dev <phetadmin@gmail.com>
2024-02-08 03:24:28 -0700

    update copyright dates from daily grunt work

--- js/fair-share/view/FairShareNotepadNode.ts
+++ js/fair-share/view/FairShareNotepadNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * Adds a RectangularRadioButtonGroup to the NotepadNode for the Fair Share screen
