8fe70dc2a3a180c77148627eb4dd458fd147ac6d
Marla Schulz <marla.schulz@colorado.edu>
2024-02-14 09:59:31 -0700

    Set phetioState to false, see: https://github.com/phetsims/mean-share-and-balance/issues/152

--- js/balance-point/model/BalancePointModel.ts
+++ js/balance-point/model/BalancePointModel.ts
@@ -12,13 +12,18 @@ import { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import SoccerModel, { SoccerModelOptions } from '../../../../soccer-common/js/model/SoccerModel.js';
 import BalancePointSceneModel from './BalancePointSceneModel.js';
 import WithRequired from '../../../../phet-core/js/types/WithRequired.js';
+import { optionize } from '../../../../phet-core/js/imports.js';
 
 type SelfOptions = EmptySelfOptions;
 type BalancePointModelOptions = SelfOptions & WithRequired<SoccerModelOptions<BalancePointSceneModel>, 'tandem'>;
 
 export default class BalancePointModel extends SoccerModel<BalancePointSceneModel> {
 
-  public constructor( options: BalancePointModelOptions ) {
+  public constructor( providedOptions: BalancePointModelOptions ) {
+
+    const options = optionize<BalancePointModelOptions, SelfOptions, SoccerModelOptions<BalancePointSceneModel>>()( {
+      phetioState: false
+    }, providedOptions );
 
     const sceneModel = new BalancePointSceneModel( {
       tandem: options.tandem.createTandem( 'sceneModel' )
