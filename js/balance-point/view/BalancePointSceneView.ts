4e4cf2d32fd4bd01e0b647d464b725128def42e1
phet-dev <phetadmin@gmail.com>
2024-02-17 03:19:27 -0700

    update copyright dates from daily grunt work

--- js/balance-point/view/BalancePointSceneView.ts
+++ js/balance-point/view/BalancePointSceneView.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 /**
  * Displays the soccer balls and data points for at scene in the balance point screen.
  *
