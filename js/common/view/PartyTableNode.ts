508eb32fa6d8ed1529e0fda80800441e90b772e9
Marla Schulz <marla.schulz@colorado.edu>
2024-02-20 13:41:40 -0700

    Update partyTable artwork, see: https://github.com/phetsims/mean-share-and-balance/issues/145

--- js/common/view/PartyTableNode.ts
+++ js/common/view/PartyTableNode.ts
@@ -8,18 +8,28 @@
  */
 
 import meanShareAndBalance from '../../meanShareAndBalance.js';
-import { Node, Image, NodeOptions } from '../../../../scenery/js/imports.js';
-import partyTable_png from '../../../images/partyTable_png.js';
+import { Image, Node, NodeOptions } from '../../../../scenery/js/imports.js';
 import MeanShareAndBalanceConstants from '../MeanShareAndBalanceConstants.js';
+import partyTable_svg from '../../../images/partyTable_svg.js';
+import { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import { optionize } from '../../../../phet-core/js/imports.js';
+import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
+
+type SelfOptions = EmptySelfOptions;
+type PartyTableNodeOptions = SelfOptions & StrictOmit<NodeOptions, 'children'>;
 
 export default class PartyTableNode extends Node {
 
-  public constructor( providedOptions?: NodeOptions ) {
-    const partyTableImage = new Image( partyTable_png, { scale: 0.32 } );
-    super( {
-      children: [ partyTableImage ],
-      y: MeanShareAndBalanceConstants.PARTY_TABLE_Y
-    } );
+  public constructor( providedOptions?: PartyTableNodeOptions ) {
+    const partyTableImage = new Image( partyTable_svg, { maxWidth: 780 } );
+
+    const options = optionize<PartyTableNodeOptions, SelfOptions, NodeOptions>()(
+      {
+        children: [ partyTableImage ],
+        y: MeanShareAndBalanceConstants.PARTY_TABLE_Y
+      },
+      providedOptions );
+    super( options );
   }
 }
 
