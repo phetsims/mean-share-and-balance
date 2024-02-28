0854cb83fb6b08a27329c13afd83ba3862e08f50
phet-dev <phetadmin@gmail.com>
2024-02-28 03:19:18 -0700

    update copyright dates from daily grunt work

--- js/balance-point/view/BalanceBeamNode.ts
+++ js/balance-point/view/BalanceBeamNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * The Balance Beam in the notepad representation plots data points along the x-axis and allows users to move the balance
--- js/balance-point/view/BalancePointNotepadNode.ts
+++ js/balance-point/view/BalancePointNotepadNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * The BalancePointNotepadNode adds a mean readout and check button to the notepad node for the balance point screen.
