31a60ba15d3aa7b2dca00731c87c3ebc72cacc6d
jbphet <jpblanco@yahoo.com>
2024-04-22 13:22:36 -0600

    improve PDOM structure, see https://github.com/phetsims/greenhouse-effect/issues/398

--- js/level-out/LevelOutScreen.ts
+++ js/level-out/LevelOutScreen.ts
@@ -16,6 +16,9 @@ import meanShareAndBalance from '../meanShareAndBalance.js';
 import MeanShareAndBalanceStrings from '../MeanShareAndBalanceStrings.js';
 import Screen, { ScreenOptions } from '../../../joist/js/Screen.js';
 import SliderControlsAndBasicActionsKeyboardHelpContent from '../../../scenery-phet/js/keyboard/help/SliderControlsAndBasicActionsKeyboardHelpContent.js';
+import { Image } from '../../../scenery/js/imports.js';
+import levelOutHomeScreenIcon_svg from '../../images/levelOutHomeScreenIcon_svg.js';
+import ScreenIcon from '../../../joist/js/ScreenIcon.js';
 
 type SelfOptions = EmptySelfOptions;
 
@@ -25,6 +28,10 @@ export default class LevelOutScreen extends Screen<LevelOutModel, LevelOutScreen
   public constructor( providedOptions: LevelOutScreenOptions ) {
     const options = optionize<LevelOutScreenOptions, SelfOptions, ScreenOptions>()( {
       name: MeanShareAndBalanceStrings.screen.levelOutStringProperty,
+      homeScreenIcon: new ScreenIcon( new Image( levelOutHomeScreenIcon_svg ), {
+        maxIconWidthProportion: 1,
+        maxIconHeightProportion: 1
+      } ),
       backgroundColorProperty: MeanShareAndBalanceColors.levelOutScreenBackgroundColorProperty,
       createKeyboardHelpNode: () => new SliderControlsAndBasicActionsKeyboardHelpContent()
     }, providedOptions );
