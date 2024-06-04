249f685b9cf8d8c7b79f6d4457e556696c3d6f5c
phet-dev <phetadmin@gmail.com>
2024-06-04 03:17:45 -0600

    update copyright dates from daily grunt work

--- js/common/view/SyncIcon.ts
+++ js/common/view/SyncIcon.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 import { Node } from '../../../../scenery/js/imports.js';
 import meanShareAndBalance from '../../meanShareAndBalance.js';
