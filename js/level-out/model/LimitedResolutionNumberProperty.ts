6b8c0ccef0aecad270a2714486cefc508556373d
phet-dev <phetadmin@gmail.com>
2024-07-02 03:25:51 -0600

    update copyright dates from daily grunt work

--- js/level-out/model/LimitedResolutionNumberProperty.ts
+++ js/level-out/model/LimitedResolutionNumberProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * LimitedResolutionNumberProperty is an extension of the NumberProperty class that limits the resolution of the numeric
