83402c2193ca244624a26be4a480f215be6d085e
jbphet <jpblanco@yahoo.com>
2024-07-15 15:07:45 -0600

    use new common code version of CrossFadeSoundClip, see https://github.com/phetsims/mean-share-and-balance/issues/303

--- js/level-out/view/WaterLevelSoundPlayer.ts
+++ js/level-out/view/WaterLevelSoundPlayer.ts
@@ -11,12 +11,12 @@ import ValueChangeSoundPlayer from '../../../../tambo/js/sound-generators/ValueC
 import Range from '../../../../dot/js/Range.js';
 import meanShareAndBalance from '../../meanShareAndBalance.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
-import CrossFadeSoundClip from './CrossFadeSoundClip.js';
 import soundManager from '../../../../tambo/js/soundManager.js';
 import glassLevelSoundA_mp3 from '../../../sounds/glassLevelSoundA_mp3.js';
 import glassLevelSoundB_mp3 from '../../../sounds/glassLevelSoundB_mp3.js';
 import SoundUtils from '../../../../tambo/js/SoundUtils.js';
 import Utils from '../../../../dot/js/Utils.js';
+import CrossFadeSoundClip from '../../../../tambo/js/sound-generators/CrossFadeSoundClip.js';
 
 class WaterLevelSoundPlayer extends ValueChangeSoundPlayer {
 
