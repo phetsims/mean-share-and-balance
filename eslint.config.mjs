08ecdcd3ba4d641ea56f60d6bcc563fcece3ff5e
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:28 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for mean-share-and-balance.
+ * ESLint configuration for mean-share-and-balance.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
