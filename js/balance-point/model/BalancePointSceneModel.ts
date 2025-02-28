19aa2a4b90a39f54e4c31ef8d9310052d2c24ffd
phet-dev <phetadmin@gmail.com>
2025-02-28 03:38:54 -0700

    update copyright dates from daily grunt work

--- js/balance-point/model/BalancePointSceneModel.ts
+++ js/balance-point/model/BalancePointSceneModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * The BalancePointSceneModel has the soccer ball information for the balance point screen as well as the information
