2b600723cc2649aea0f3ea1b2ea3be5f90eaed9b
phet-dev <phetadmin@gmail.com>
2025-03-06 03:37:07 -0700

    update copyright dates from daily grunt work

--- js/fair-share/view/AllocationModeSoundGenerator.ts
+++ js/fair-share/view/AllocationModeSoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * AllocationModeSoundPlayer is a sound generator that produces sounds when the allocation mode of the Fair Share
