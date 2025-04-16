f9f65a9c5ee71f06f122498b2bb577d51622b361
Sam Reid <reids@colorado.edu>
2025-04-16 09:52:13 -0600

    Move imports after file doc, see https://github.com/phetsims/perennial/issues/447

--- js/level-out/view/LevelOutKeyboardHelpNode.ts
+++ js/level-out/view/LevelOutKeyboardHelpNode.ts
@@ -1,16 +1,17 @@
 // Copyright 2024, University of Colorado Boulder
-import SliderControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SliderControlsKeyboardHelpSection.js';
-import SpinnerControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SpinnerControlsKeyboardHelpSection.js';
+
 /**
  * The keyboard help content for the Level Out screen includes slider controls and spinner controls.
  *
  * @author Marla Schulz (PhET Interactive Simulations)
  *
  */
+
+import SliderControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SliderControlsKeyboardHelpSection.js';
+import SpinnerControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SpinnerControlsKeyboardHelpSection.js';
 import MeanShareAndBalanceKeyboardHelpNode from '../../common/view/MeanShareAndBalanceKeyboardHelpNode.js';
 import meanShareAndBalance from '../../meanShareAndBalance.js';
 import MeanShareAndBalanceStrings from '../../MeanShareAndBalanceStrings.js';
-
 
 export default class LevelOutKeyboardHelpNode extends MeanShareAndBalanceKeyboardHelpNode {
 
